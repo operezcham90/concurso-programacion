41f59a00d0b540b7e43a958ed6f79d85d2ac4d5b
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-04-19 13:26:02 -0600

    ✏

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const cp = require('child_process')
 const http = require('http')
 const fs = require('fs')
@@ -19,7 +21,7 @@ function raiz(respuesta) {
 }
 
 function version(respuesta) {
-    let mensaje = ''
+    let mensaje = '⛔'
     respuesta.writeHead(200, { 'Content-Type': 'application/json' })
     cp.exec('gcc --version', (error, exito, fracaso) => {
         mensaje = error | exito | fracaso
