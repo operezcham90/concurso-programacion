2e797b645d7bc21cd9479e5bbfe83e772d6537ea
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-04-19 13:19:17 -0600

    ✏

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process')
 const http = require('http')
 const fs = require('fs')
 
@@ -7,7 +8,25 @@ const servidor = http.createServer(responder)
 servidor.listen(PORT)
 
 function responder(solicitud, respuesta) {
+    if (solicitud.url === '/' && solicitud.method === 'GET') raiz(respuesta)
+    if (solicitud.url === 'gcc/version' && solicitud.method === 'GET') version(respuesta)
+}
+
+function raiz(respuesta) {
     respuesta.writeHead(200, { 'Content-Type': 'text/html' })
     const texto = fs.readFileSync('root.html', 'utf8')
     respuesta.end(texto)
+}
+
+function version(respuesta) {
+    let mensaje = '⛔'
+    respuesta.writeHead(200, { 'Content-Type': 'application/json' })
+    exec('gcc --version', (error, exito, fracaso) => {
+        if (error) return
+        mensaje = exito | fracaso
+        const texto = JSON.parse({
+            mensaje: mensaje
+        })
+        respuesta.end(texto)
+    })
 }
