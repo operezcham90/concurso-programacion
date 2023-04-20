bb7e1701d3247e61d43d508d257270441cab622f
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-04-20 11:49:27 -0600

    📍

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ function responder(solicitud, respuesta) {
     else if (solicitud.url === '/codemirror.js' && solicitud.method === 'GET') rutinas(respuesta, '🐚')
     else if (solicitud.url === '/favicon.ico' && solicitud.method === 'GET') icono(respuesta)
     else if (solicitud.url === '/gcc/version' && solicitud.method === 'GET') version(respuesta)
+    else if (solicitud.url === '/ip/direccion' && solicitud.method === 'GET') direccion(solicitud, respuesta)
     else respuesta.end('✌')
 }
 
@@ -54,7 +55,7 @@ function icono(respuesta) {
 }
 
 function version(respuesta) {
-    let numero = '⛔'
+    let numero = '0.0.0'
     respuesta.writeHead(200, { 'Content-Type': 'application/json' })
     cp.exec('gcc -dumpversion', (error, exito, fracaso) => {
         if (exito) numero = exito.trim()
@@ -64,4 +65,14 @@ function version(respuesta) {
         const texto = JSON.stringify(datos)
         respuesta.end(texto)
     })
+}
+
+function direccion(solicitud, respuesta) {
+    respuesta.writeHead(200, { 'Content-Type': 'application/json' })
+    const ip = solicitud.connection.remoteAddress
+    const datos = {
+        ip
+    }
+    const texto = JSON.stringify(datos)
+    respuesta.end(texto)
 }
